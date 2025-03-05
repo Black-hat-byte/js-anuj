c769d85d5d4a5fad48dc7b5a01f2e0f47374408c
Black-hat-byte <anujpratapmaurya5@gmail.com>
2025-03-05 16:21:53 +0000

    container config done

---
+++ basicofjava/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+// alert (3+3);
+//  we are using node js ,not browser so here we can't use this alert
+// string
+// number
+// null
+// undefined
+// boolean
+// bigint=>when number is too big,usually not in use
+// symbol=>uniqueness findout 
+
+// object
+let age=63
+console.log(typeof age);
+console.log(typeof null)
+// this give the type of object but some people consider it as laguage error while some people consider it as way of javascript
+console.log(typeof undefined)
+// this will give undefined type
