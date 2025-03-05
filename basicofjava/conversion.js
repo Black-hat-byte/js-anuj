9d8f1492d6180b60e08407a133e4b1f43e62dff1
Black-hat-byte <anujpratapmaurya5@gmail.com>
2025-03-05 16:37:57 +0000

    container config done

---
+++ basicofjava/conversion.js
@@ -0,0 +1,16 @@
+let score= 33
+console.log(typeof score);
+console.log(score)
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber)
+console.log(valueInnumber)
+// "33"=>33
+// "33abc"=>NaN(not a number)
+// true=>1,false=>0
+
+let name="1"
+let valueinboolean = Boolean(name)
+console.log(typeof valueinboolean)
+console.log(name)
+// 1=>true, 0=>false
